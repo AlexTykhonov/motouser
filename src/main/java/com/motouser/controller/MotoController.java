b6fc769a289f32c684f453b9dc03776e191c6af9
AlexTykhonov <alexey.tykhonov@gmail.com>
2020-02-13 22:50:05 +0200

    bug fix 2 maven plugin error updated2 deleted driver class

--- src/main/java/com/motouser/controller/MotoController.java
+++ src/main/java/com/motouser/controller/MotoController.java
@@ -3,9 +3,12 @@ package com.motouser.controller;
 import com.motouser.model.Moto;
 import com.motouser.repository.MotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.util.BsonUtils;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.sql.SQLOutput;
 
 @RestController
 @CrossOrigin
@@ -18,6 +21,13 @@ MotoRepository motoRepository;
 @GetMapping
     public Flux<Moto> getAllMoto () {
     return motoRepository.findAll();
+}
+
+@GetMapping ("/{id}")
+public  Mono <Moto> getTheMoto(@PathVariable String id) {
+    System.out.println(" *********************** "+id);
+    motoRepository.findById(id).subscribe(x-> System.out.println(x));
+return motoRepository.findById(id);
 }
 
 @PostMapping
